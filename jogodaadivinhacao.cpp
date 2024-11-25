2448853b665a04ce5007f45ca9135e9888b5d91d
SunaUezuri <wesley.jane.santos@gmail.com>
2024-11-25 10:35:57 -0300

    Iniciando o jogo com outputs

---
+++ jogodaadivinhacao.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<stdio.h>
+//Dizendo ao programa que estamos utilizando as funções padrões
+using namespace std;
+
+//Função principal que guarda o código ao ser iniciado
+int main (){
+    //std::cout == print
+    cout << "**************************************" << endl; //endl == quebra de linha
+    cout << "* Bem-Vindos ao jogo da adivinhacao! *" << endl;
+    cout << "**************************************" << endl;
+}
